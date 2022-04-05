13d9a737860cb9dc2d538179eac10a344fc03151
Ryan Henderson <wrhenders@gmail.com>
2022-04-05 14:37:43 -0500

    added tests for methods

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,10 +1,12 @@
 const assert = require("assert");
 const ganache = require("ganache-cli");
 const Web3 = require("web3");
+const { interface, bytecode } = require("../compile");
 
 const web3 = new Web3(ganache.provider());
 
 let accounts;
+let inbox;
 
 beforeEach(async () => {
   // Get a list of all accounts
@@ -13,10 +15,24 @@ beforeEach(async () => {
   // Use one of those accounts to deploy
 
   // The contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ["Hi There!"] })
+    .send({ from: accounts[0], gas: "1000000" });
 });
 
 describe("Inbox", () => {
   it("deploys a contract", () => {
-    console.log(accounts);
+    assert.ok(inbox.options.address);
+  });
+
+  it("has a default message", async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, "Hi There!");
+  });
+
+  it("can change the message", async () => {
+    await inbox.methods.setMessage("Bye").send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, "Bye");
   });
 });
