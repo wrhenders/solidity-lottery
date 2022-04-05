e20cf119712e9900166e766ffa9d91dd06f29d19
Ryan Henderson <wrhenders@gmail.com>
2022-04-05 15:14:45 -0500

    added deploy to rinkeby code

---
+++ deploy.js
@@ -0,0 +1,29 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
+
+require("dotenv").config();
+
+const key = process.env.META_KEY;
+
+const provider = new HDWalletProvider(
+  key,
+  "https://rinkeby.infura.io/v3/49c1b6f4d5e24da3a9871abaebb9a071"
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from accout", accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ["Hi There!"] })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
+  provider.engine.stop();
+};
+
+deploy();
